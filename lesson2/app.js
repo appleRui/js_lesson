0c5ae6fc936212c8db0f62438c5743092d01b556
appleRui <apple31581@gmail.com>
2020-12-12 16:23:22 +0900

    end 18 dotinstall

--- lesson2/app.js
+++ lesson2/app.js
@@ -108,3 +108,28 @@ for (let i = 0; i < scores.length; i++){
     console.log(x); // [5, 2]
     console.log(y); // [1, 2]
 }
+
+{
+    const str = 'hello';
+
+  console.log(str.length);
+
+  // str.substring(開始位置, 終了位置);
+  console.log(str.substring(2, 4));
+
+  console.log(str[1]);
+}
+
+{
+  const d = [2019, 11, 14];
+
+  console.log(d.join('/'));
+  // console.log(d.join(''));
+
+  const t = '17:08:24';
+  // console.log(t.split(':'));
+  const [hour, minute, second] = t.split(':');
+  console.log(hour);
+  console.log(minute);
+  console.log(second);
+}
