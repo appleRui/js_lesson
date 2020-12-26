c4e38b9a69864c58734e07cc4a29d7488d4eafde
appleRui <apple31581@gmail.com>
2020-12-26 22:46:06 +0900

    edit app.js idenx.html

--- onCheckfunction/app.js
+++ onCheckfunction/app.js
@@ -1,10 +1,10 @@
 function oncheck(){
     const fieldtime = document.querySelector('input[type="time"]');
-    const chk = document.getElementById("checkbox1");
-    // const chk = document.querySelector('input[type="time"]');
+    const chkbx = document.getElementById("checkbox");
+
     console.log(fieldtime);
-    if (chk.checked){
-        alert("変更します?");
+    if (chkbx.checked){
+        // alert("変更します?");
         fieldtime.value = '15:30';
         fieldtime.classList.add('taimeclickoff');
     }else{
